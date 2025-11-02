53b3bcc1f8047e442b5aa5f03052a56901417cac
Luis-777777 <A01714017@tec.mx>
2025-11-01 20:07:31 -0600

    Cambios en el codigo

--- Operacion.c
+++ Operacion.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-int main(){
-int num1 = 12;
-int num2 = 3;
-int op1 = suma(num1,num2);
-printf("%i",op1);
-int op2 = resta(num1,num2);
-13
-printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
-return 0;
+
+// --- Funciones que faltaban ---
+int suma(int a, int b) {
+    return a + b;
+}
+
+int resta(int a, int b) {
+    return a - b;
+}
+
+int multi(int a, int b) {
+    return a * b;
+}
+// ---------------------------------
+
+int main() {
+    int num1 = 12;
+    int num2 = 3;
+
+    int op1 = suma(num1, num2);
+    printf("Suma: %i\n", op1); // Se corrigió el printf
+
+    int op2 = resta(num1, num2);
+    printf("Resta: %i\n", op2); // Se corrigió la línea 10 y 11
+
+    int op3 = multi(num1, num2);
+    printf("Multiplicacion: %i\n", op3);
+
+    return 0;
 }
