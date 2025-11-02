fb10da532a090f642a7f744652401e273491ae53
Vale-ce <a01713760tec.mx>
2025-11-01 20:50:09 -0600

    Eliminamos el 13, cambiamos la linea de suma, y se elimino una tercera operacion

---
+++ VersParticipante.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+int num1 = 12;
+int num2 = 3;
+int op1 = num1 + num2;
+printf("%i",op1);
+printf("\n");
+int op2 = num1 - num2;
+printf("%i",op2);
+printf("\n");
+return 0;
+}
